fe94666f4927bab572c9366a8774dc0e2448ae97
Le Tu Quyen <quyenlt@dgroup.co>
2018-11-22 14:43:42 +0700

    Add fu

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './App.css';
 import axios from 'axios';
 import InteractiveMap from './components/InteractiveMap/InteractiveMap.js';
 class App extends Component {
-render() {
-    return (
-    <div>
-      <InteractiveMap/>
-      <button onClick={this.onClick}>Click me</button>
-    </div>
-    );
-}
+    render() {
+        return (
+            <div>
+                <InteractiveMap />
+                <button onClick={this.onClick}>Click me Fuck you</button>
+            </div>
+        );
+    }
 
-onClick(ev) {
-    console.log("Sending a POSt API Call !!!");
-    axios.get('http://127.0.0.1:8000/snippets/1')
-    .then(res => {
-            console.log((res.data));
-    })
-}
+    onClick(ev) {
+        console.log("Sending a POSt API Call !!!");
+        axios.get('http://127.0.0.1:8000/snippets/1')
+            .then(res => {
+                console.log((res.data));
+            })
+    }
 }
 export default App;
